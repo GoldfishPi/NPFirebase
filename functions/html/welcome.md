527a3f3cb6f8d426d693dfc546ea69a1f895a440
Erik Badger <erikbadger777@gmail.com>
2019-10-04 19:19:55 -0500

    initial commit

---
+++ functions/html/welcome.md
@@ -0,0 +1,5 @@
+
+# Header
+Congradulations you've successfully signed up for Nomad Pit Stop!
+
+You can get started by exploring pit stops near you or checking out some of the latest blog posts. 
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,47 @@
-import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const fs=require('fs'); 
+const nodemailer = require('nodemailer');
+
+admin.initializeApp();
+
+const gmailEmail = "erik@nomadpitstops.com";
+const gmailPassword = "raspberry0258#";
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
+});
+
+var htmlmail = fs.readFileSync(__dirname+'/../html/welcome.html','utf-8').toString();
+
+export const sendWelcomeEmailTrigger = functions.auth
+    .user()
+    .onCreate(async (user:any) => {
+        const recipent_email = user.email; 
+        return await sendWelcomeEmail(recipent_email);
+
+    });
+
+export const sendWelcomeEmail = async (email:string) => {
+    const mailOptions = {
+        from: `"Erik Badger" <${gmailEmail}>`,
+        to: email,
+        subject: 'Welcome To Nomad Pit Stops',
+        html: htmlmail
+    };
+
+    try {
+        await mailTransport.sendMail(mailOptions);
+        console.log('mail send');
+
+    } catch(error) {
+        console.error('There was an error while sending the email:', error);
+    }
+    return null; 
+}
+
+export const logEmailString = () => console.log(htmlmail);
